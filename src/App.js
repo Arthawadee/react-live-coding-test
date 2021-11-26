918b195bbc9a371ba0d492011e5fb13a34240158
Udayananond, Arthawadee <por_25418@hotmail.com>
2021-11-26 12:50:23 +0700

    First test finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
 import Home from "./Home";
-import { Route, NavLink, HashRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import PokeDex from "./PokeDex";
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Route exact path="/" component={Home}/>
-        <Route path="/pokedex" component={PokeDex}/>
-      </div>
-      
-    </HashRouter>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/pokedex" component={PokeDex} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
